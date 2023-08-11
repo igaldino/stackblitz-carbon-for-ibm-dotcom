8f0862bfb0a443839726a21ec5aa076b0d76cbc8
Isaque Galdino <isaque@br.ibm.com>
2023-08-11 11:38:47 -0300

    Move to Angular 15

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import '@carbon/ibmdotcom-web-components/es/components/dotcom-shell/index';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ibm-shell';
+}
